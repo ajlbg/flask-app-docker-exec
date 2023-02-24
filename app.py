b52f0c9aee88d12742559cc0a4fdf74af90f7d5f
ajlbg <120402175+ajlbg@users.noreply.github.com>
2023-02-24 15:59:54 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-  return render_template('index.html')
-  
-  if __name__ == '__main__':
+    return render_template('index.html')
+
+if __name__ == "__main__":
     app.run()
